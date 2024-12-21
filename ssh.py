82ce1f67d2118d517e197988d5b5193a06e78300
Sourjya Basu <sourjyabose@hotmail.com>
2024-12-21 09:03:57 +0530

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,13 +1,25 @@
 import socket
 import sys
-s=socket.socket()
+import time
+s=socket.socket(family=socket.AF_INET6)
 s1=socket.socket();
 s1.connect((sys.argv[1],5000))
-s.bind(('',8000))
+s.bind(('',int(sys.argv[2])))
 s.listen(10)
+
 c,addr=s.accept()
+print(addr)
+i=0
 while True:
+    time.sleep(3)
     btw=c.recv(10000)
+   
+
     s1.send(btw)
+    time.sleep(3)
     wtf=s1.recv(10000)
+    
     c.send(wtf)
+    
+    print(i)
+    i=i+1
